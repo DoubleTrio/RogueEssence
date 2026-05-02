98cac13824c6e593ad3508474c95924fa71aaefc
Kace <41309226+DoubleTrio@users.noreply.github.com>
2026-05-01 21:11:39 -0400

    wip: resolve the dependency for parent form in editors

---
+++ RogueEssence.Editor.Avalonia/Views/New/DialogBoxes/MapResizeWindowView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RogueEssence.Editor.Avalonia.Views.New.DialogBoxes;
+
+public partial class MapResizeWindowView : UserControl
+{
+    public MapResizeWindowView()
+    {
+        InitializeComponent();
+    }
+}
